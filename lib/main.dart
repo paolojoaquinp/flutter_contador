ecde5785bb3fcb474a5cced945ea574ec7b00384
paolojoaquinp <paolojoaquinpintoperez@gmail.com>
2022-01-05 13:17:09 -0400

    [v1.0] Initial commit]

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// Importaciones Nuestras
+import 'package:contador/src/app.dart';
+/* import 'src/app.dart';
+ */
+void main() {
+  runApp(new MyApp());
+}
+
+
+
